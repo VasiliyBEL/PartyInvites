3f88b92c93f8eee76192842b847132848f8e0622
VasiliyBEL <vaspn001@gmail.com>
2022-03-31 08:06:20 +0300

    Created Model-GuestResponse and Views for HomeController(Index and RsvpForm). Chose specific page for started URL-address.

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -15,5 +15,10 @@ namespace PartyInvites.Controllers
             ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Доброго дня";
             return View();
         }
+
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
     }
 }
