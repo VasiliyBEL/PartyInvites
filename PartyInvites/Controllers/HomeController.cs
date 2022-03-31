20ab0847127e39472dda772a28485b98eda175ee
VasiliyBEL <vaspn001@gmail.com>
2022-03-31 14:57:07 +0300

    Created Styles.css for pages.

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -26,9 +26,13 @@ namespace PartyInvites.Controllers
         [HttpPost]
         public ViewResult RsvpForm(GuestResponse guest)
         {
-            // Нужно отправить данные нового гостя no электронной почте 
-            // организатору вечеринки.
-            return View("Thanks", guest);
+            if (ModelState.IsValid)
+                // Нужно отправить данные нового гостя по электронной почте 
+                // организатору вечеринки.
+                return View("Thanks", guest);
+            else
+                // Обнаружена ошибка проверки достоверности
+                return View();
         }
     }
 }
