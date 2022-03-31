d68b3c945511d56e4389888c20e11863d37b6a52
VasiliyBEL <vaspn001@gmail.com>
2022-03-31 13:06:24 +0300

    Visualization of views.

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PartyInvites.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,9 +17,18 @@ namespace PartyInvites.Controllers
             return View();
         }
 
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guest)
+        {
+            // Нужно отправить данные нового гостя no электронной почте 
+            // организатору вечеринки.
+            return View("Thanks", guest);
         }
     }
 }
